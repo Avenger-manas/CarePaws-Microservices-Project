dc789bcb60720e482670d735efa5f0623f19618c
Manas Rastogi <manasrastogi64@gmail.com>
2026-01-24 16:50:00 +0530

    Added Eureka Server to mono-repo

---
+++ eureka-server/EurekaServer-Pawscare/src/test/java/EurekaRegister/EurekaServer_Pawscare/EurekaServerPawscareApplicationTests.java
@@ -0,0 +1,13 @@
+package EurekaRegister.EurekaServer_Pawscare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerPawscareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
