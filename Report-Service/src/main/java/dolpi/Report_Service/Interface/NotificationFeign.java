7400bf6816b0dc367983e67e3b9ea5d490ca6731
Manas Rastogi <manasrastogi64@gmail.com>
2026-02-06 14:25:28 +0530

    Update NotificationFeign to remove URL configuration
    Removed hardcoded URL from Feign client definition.

--- Report-Service/src/main/java/dolpi/Report_Service/Interface/NotificationFeign.java
+++ Report-Service/src/main/java/dolpi/Report_Service/Interface/NotificationFeign.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="CAREPAWS-NOTIFICATION-SERVICE",url = "http://localhost:8086")
+@FeignClient(name="CAREPAWS-NOTIFICATION-SERVICE")
 public interface NotificationFeign {
     @PostMapping("/save/save-notification")
     void savenotification(@RequestBody Notifiaction notification);
