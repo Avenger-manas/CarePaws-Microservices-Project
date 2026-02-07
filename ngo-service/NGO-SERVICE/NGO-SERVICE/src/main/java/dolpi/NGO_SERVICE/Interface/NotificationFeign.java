7508673239445cea1c2c71c97da63a34aa6fd708
Manas Rastogi <manasrastogi64@gmail.com>
2026-02-07 12:07:21 +0530

    Update NotificationFeign.java

--- ngo-service/NGO-SERVICE/NGO-SERVICE/src/main/java/dolpi/NGO_SERVICE/Interface/NotificationFeign.java
+++ ngo-service/NGO-SERVICE/NGO-SERVICE/src/main/java/dolpi/NGO_SERVICE/Interface/NotificationFeign.java
@@ -4,12 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import dolpi.NGO_SERVICE.Dto.Notification;
-
 import java.util.List;
 
-@FeignClient(name="CarePaws-Notification-Service",url = "http://localhost:8086")
+
+@FeignClient(name="CarePaws-Notification-Service") 
 public interface NotificationFeign {
     @GetMapping("/save/getify")
     public List<Notification> checkngomuncipalnotify(@RequestParam String notification);
