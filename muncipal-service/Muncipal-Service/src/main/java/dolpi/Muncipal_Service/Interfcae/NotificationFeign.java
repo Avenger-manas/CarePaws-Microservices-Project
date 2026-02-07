d5bb98ecf724485ff4779218e3e9a7f9d633a59a
Manas Rastogi <manasrastogi64@gmail.com>
2026-02-07 11:54:32 +0530

    Update NotificationFeign.java

--- muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/NotificationFeign.java
+++ muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/NotificationFeign.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
-@FeignClient(name="CarePaws-Notification-Service",url = "http://localhost:8086")
+
+@FeignClient(name="CAREPAWS-NOTIFICATION-SERVICE") 
 public interface NotificationFeign {
     @GetMapping("/save/getify")
     public List<Notification> checkngomuncipalnotify(@RequestParam String notification);
