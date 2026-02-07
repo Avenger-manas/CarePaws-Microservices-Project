863b3c36375c3e516b99cca5c481d2699c487473
Manas Rastogi <manasrastogi64@gmail.com>
2026-02-07 12:00:44 +0530

    Remove URL comment for Eureka discovery
    Removed comment about URL to allow Eureka discovery to find IP automatically.

--- muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/ReportFeign.java
+++ muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/ReportFeign.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// 'url' ko hata diya gaya hai taaki Eureka discovery automatically IP dhund le
+
 @FeignClient(name="NOTIFICATION-SERVICE") 
 public interface ReportFeign {
     @PostMapping("/fetch/GetReport")
