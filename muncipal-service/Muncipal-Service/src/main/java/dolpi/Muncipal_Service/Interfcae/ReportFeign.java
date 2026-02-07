b5bd42b59c63aa232f555027445d618235bbfe9c
Manas Rastogi <manasrastogi64@gmail.com>
2026-02-07 13:01:54 +0530

    Update ReportFeign.java

--- muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/ReportFeign.java
+++ muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/ReportFeign.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @FeignClient(name="NOTIFICATION-SERVICE") 
-public interface ReortFeign {
+public interface ReportFeign {
     @PostMapping("/fetch/GetReport")
     public ReportEntity fetchreport(@RequestParam String id, @RequestBody UserNotification userNotification);
 }
