01e614894e3fab8f434ac489ddaa6f387e850272
Manas Rastogi <manasrastogi64@gmail.com>
2026-02-07 13:12:00 +0530

    Fix package imports in ReportFeign interface
    Updated package imports to reflect correct paths for Muncipal_Service DTOs.

--- muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/ReportFeign.java
+++ muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/ReportFeign.java
@@ -1,12 +1,11 @@
-package dolpi.NGO_SERVICE.Interface;
+package dolpi.Muncipal_Service.Interfcae; // Yeh sahi package hai
 
-import dolpi.NGO_SERVICE.Dto.ReportEntity;
-import dolpi.NGO_SERVICE.Dto.UserNotification;
+import dolpi.Muncipal_Service.DTO.ReportEntity; // NGO wala nahi, Muncipal wala DTO
+import dolpi.Muncipal_Service.DTO.UserNotification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @FeignClient(name="NOTIFICATION-SERVICE") 
 public interface ReportFeign {
