d714a505f488b97ba32562a5e8e327cd3e68dde4
Manas Rastogi <manasrastogi64@gmail.com>
2026-02-07 12:09:30 +0530

    Update ReportFeign.java

--- muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/ReportFeign.java
+++ muncipal-service/Muncipal-Service/src/main/java/dolpi/Muncipal_Service/Interfcae/ReportFeign.java
@@ -1,15 +1,14 @@
-package dolpi.Muncipal_Service.Interfcae;
+package dolpi.NGO_SERVICE.Interface;
 
-import dolpi.Muncipal_Service.DTO.ReportEntity;
-import dolpi.Muncipal_Service.DTO.UserNotification;
+import dolpi.NGO_SERVICE.Dto.ReportEntity;
+import dolpi.NGO_SERVICE.Dto.UserNotification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @FeignClient(name="NOTIFICATION-SERVICE") 
-public interface ReportFeign {
+public interface ReortFeign {
     @PostMapping("/fetch/GetReport")
     public ReportEntity fetchreport(@RequestParam String id, @RequestBody UserNotification userNotification);
 }
